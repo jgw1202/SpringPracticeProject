63bd744976c1a32f9c654df525fb0d78d99c767f
JungGunWoo <jgw1202@naver.com>
2024-02-09 14:33:28 +0900

    commit 7 - Component

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan ( // 다른 예제에 빈 코드를 그냥 남겨두기 위해 스캔 필터링
+        //basePackages = "hello.core.member", // 해당 패키지 하위 클래스만 필터링
+        //basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(
+                type = FilterType.ANNOTATION, classes = Configuration.class))
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,7 +1,12 @@
 package hello.core.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component // 스프링 빈 자동 등록 컴포넌트
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository;
-
+    @Autowired // 의존성 주입
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
